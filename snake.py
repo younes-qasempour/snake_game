b1c795e875a99d6cc8dc350d1dd1b315ccf0eaeb
younes_qasempour <qasempouryounes@gmail.com>
2025-02-15 12:53:38 +0330

    Final code

--- snake.py
+++ snake.py
@@ -6,6 +6,7 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
 
     def __init__(self):
@@ -15,11 +16,19 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -29,17 +38,17 @@ class Snake:
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        if self.head.direction != DOWN:
+        if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
     def down(self):
-        if self.head.direction != UP:
+        if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
     def left(self):
-        if self.head.direction != RIGHT:
+        if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
     def right(self):
-        if self.head.direction != LEFT:
+        if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
